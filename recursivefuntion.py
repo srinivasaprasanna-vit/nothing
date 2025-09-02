8a62c46813726686707f3284a328d1da5c133d9a
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-02 11:33:22 +0530

    Create recursivefuntion.py

---
+++ recursivefuntion.py
@@ -0,0 +1,11 @@
+import sys
+sys.setrecursionlimit(10**8)
+a=float(input())
+
+def factorial(a):
+    if a==0 or a==1:
+        return 1
+    else:
+        return a*factorial(a-1)
+
+print(factorial(a))
