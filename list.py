533e9d991ccae6a8b218d93033a3ef5833b0d830
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-08 10:50:01 +0530

    Update list.py

--- list.py
+++ list.py
@@ -9,3 +9,25 @@ print("Accessing the elements of list with negative indexing:", L2[-2])
 L3 = [['Geeks', 'For'], ['Geeks']]
 print("\nMulti Dimensional List: ", L3)
 print("Accessing Multi Dimensional List:",L3[0][1])
+
+#List slicing
+L4= [10, 20, 14, 25, 30, 45, 50]
+print("Slicing a List:",L4[:2],L4[1:3],L4[1:4],L4[4:])
+
+#List functions
+L5 = [10, 20, 14, 25, 30, 45, 50]
+print("list:",L5)
+L5.append(120)
+print("Append:",L5)
+L5.clear()
+print("Clear:",L5)
+L5 = L4.copy()
+print("Copy:",L5)
+print("count:",L5.count(20))
+L5.extend(L4)
+print("Extend:",L5)
+print("count:",L5.count(20))
+print("Index:",L5.index(25))
+L7 = [10, 20, 30, 40, 50]
+L7.pop(3)
+print("Pop:",L7)
