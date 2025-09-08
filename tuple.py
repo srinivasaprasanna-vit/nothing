54a7cfb4d581f265aa664cdb0d1a9379ea317c8d
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-08 11:23:11 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -1,4 +1,10 @@
-tup=(1,2,3,4,5,6,7,8)
+tup=(10,20,30,40,50,60,70,80,80+10)
 print("Tuple:",tup)
-print("Accessing elements of tuple")
+print("Accessing elements of tuple:",tup[3])
+print("Slicing tuple:",tup[1:],tup[4:9])
+print("len of tuple:",len(tup))
+
+num=[10,20,30,40,50]
+t1=num
+print(num)
 
