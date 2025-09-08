65ed8855466d444c80cc5418abbb562c56e6d4d0
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-08 10:43:42 +0530

    .

---
+++ list.py
@@ -0,0 +1,11 @@
+L1 = []
+print("Blank List: ", L1)
+
+L2 = [10,20,14]
+print("\nList of numbers: ", L2)
+print("Accessing Elements of list:", L2[1])
+print("Accessing the elements of list with negative indexing:", L2[-2])
+
+L3 = [['Geeks', 'For'], ['Geeks']]
+print("\nMulti Dimensional List: ", L3)
+print("Accessing Multi Dimensional List:",L3[0][1])
