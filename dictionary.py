9f6ff1a9a0ee98c0d229fc9738266a70c5237c59
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-09 10:34:38 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -9,3 +9,26 @@ print(my_dict.get(1))
 d1={"name":"John", 1:[1,2,3]}
 print(d1)
 print(d1.get(1))
+
+
+d2=dict({1:"apple",2:"ball"})
+print(d2)
+
+d3=dict([(1,"apple"),(2,"ball")])
+print(d3)
+
+x=d2.items()
+
+print(x)
+
+y=d2.keys()
+print(y)
+
+z=d2.values()
+print(z)
+
+d3.update({3:"cat"})
+print(d2)
+
+d2[1]="potato"
+print(d2) 
