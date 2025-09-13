1d95d5f604baad563703ac727c09b96fa48b5a9c
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-13 11:30:51 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,5 +1,4 @@
 t=input()
-
 l=[]
 for i in t:
    if i!=" ":
@@ -7,11 +6,13 @@ for i in t:
 
 print('List1: ',l)
 r=int(input())
-l1=l.remove(r)
-print('List after removal: ',l1)
+l.remove(r)
+print('List after removal: ',l)
 a=input()
 l2=[]
 for i in a:
     if i!=" ":
         l2.append(int(i))
 print(l2)
+l.extend(l2)
+print('Final List: ',l)
