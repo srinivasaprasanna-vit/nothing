93f808fb78357701721a75ad574da9bbb3022777
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-13 10:09:48 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,6 +1,17 @@
-def func(a, b=5, c=10):
+'''def func(a, b=5, c=10):
     print('a is', a, 'and b is', b, 'and c is', c)
  
 func(3, 7)
 func(25, c = 24)
-func(c = 50, a = 100)
+func(c = 50, a = 100)'''
+
+'''t=(1,)
+t+=(3,4)
+print("Tuple:" ,t)
+print("Max value:",max(t))'''
+
+
+a=(1,2,3)
+b=('A','B','C')
+c=zip(a,b)
+print(tuple(c))
