43d2a61c53e5e2ae1cb8c099f67e721e47366a1d
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-15 10:49:44 +0530

    Update ticketing.py

--- ticketing.py
+++ ticketing.py
@@ -1,18 +1,17 @@
 t=10
 i=0
 
-while(i<t):
+while(t>0):
+    
     n=int(input())
-    print(f"Booking {n} tickets")
-    s=str(input())
-    yes="yes"
-    no="no"
-    if s==yes and n<=(t-n-i):
-        print(f"Remaining Seats: {t-n-i}")
-        i+=n
-    elif s==no:
-        print("All seats have been booked.Thank you!")
+    if n<=t:
+        print(f"Booking {n} tickets")
+        s=str(input())
+        if s=='yes' and n<=(t-n):
+            t=t-n
+            print(f"Remaining Seats: {t}")
+        elif s=='no':
+            print("All seats have been booked.Thank you!")
     else:
         print("Not enough seats available.")
-    
 print("Thank you for using our Ticket Booking System!")
