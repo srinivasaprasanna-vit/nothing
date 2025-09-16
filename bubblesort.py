59e3c3b070a0119b67ee969b11d90119d722df86
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-09-16 10:56:01 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,10 @@
-ar=[78,12,90,23]
+ar=[12,90,23]
 
 def bubble(ar):
     n=len(ar)
     for i in range(0,n):
         for j in range(0,n-i-1):
-            if ar[j]>ar[j+1]:
+            if ar[j]>ar[j+1]: #j=0 , j+1=1
                 ar[j],ar[j+1]=ar[j+1],ar[j]
     return ar
 print(bubble(ar))
-
