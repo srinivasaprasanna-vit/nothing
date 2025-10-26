a4176525896fa1ef92382b276c334dc80572dbb4
SRINIVASA PRASANNA P G <srinivasaprasanna.p2025@vitstudent.ac.in>
2025-10-26 23:58:16 +0530

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -10,5 +10,7 @@ def binary_search(arr, key):
         else:
             high = mid - 1
     return False
+
 materials = ["Aluminum", "Brass", "Copper", "Steel", "Titanium"]
+
 print(binary_search(materials, "Steel"))
