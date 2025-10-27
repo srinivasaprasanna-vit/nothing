12777981877e5473e828751da2217943ab4e651e
SRINIVASA PRASANNA P G <223455201+srinivasaprasanna-vit@users.noreply.github.com>
2025-10-27 10:02:23 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,15 @@
+
+N = int(input())
+
+for i in range(1, N + 1):
+
+    print(' ' * (N - i), end='')
+
+
+    print('* ' * i, end='')
+
+
+    print(' ' * (2 * (N - i)), end='')
+
+
+    print('* ' * i)
